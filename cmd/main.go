550245d2b61d24f7d7f71347c478c6900709d361
TNH <thomasnhprivat@gmail.com>
2026-05-28 10:35:53 +0200

    initial

---
+++ cmd/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+
+	pb "codeberg.org/Wislow/pulse/api"
+
+	"google.golang.org/grpc"
+)
+
+type ingestorService struct {
+	pb.UnimplementedPulseServiceServer
+}
+
+func (s *ingestorService) SendLog(ctx context.Context, req *pb.LogRequest) (*pb.LogResponse, error) {
+	log.Printf("Log received:\n\tProject: %s\n\tService: %s\n\tMessage: %s", req.ProjectId, req.ServiceName, req.RequestMsg)
+
+	return &pb.LogResponse{
+		Success:     true,
+		ResponseMsg: "Log securely queued by Pulse Ingestor",
+	}, nil
+}
+
+func main() {
+	grpcPort := ":50051"
+	listener, err := net.Listen("tcp", grpcPort)
+	if err != nil {
+		log.Fatalf("Failed to bind TCP listener on %s : %v", grpcPort, err)
+	}
+
+	grcpServer := grpc.NewServer()
+
+	log.Printf("Pulse Ingestor accepting gRPC logs on port %s", grpcPort)
+	if err := grcpServer.Serve(listener); err != nil {
+		log.Fatalf("Failed to maintain gRPC server context: %v", err)
+	}
+}
